749cc71ebc4a2e64954351d2809365d87871b34f
didierlb <didierlbtuto@gmail.com>
2021-03-12 16:36:49 +0530

    update on web site ,  about page completed , home page almost completed

--- about.php
+++ about.php
@@ -25,54 +25,42 @@
       </div>
     </div> 
     
+           <section class="whysection">
+             <div class="container">
+               <div class="row">
+                 <div class="col-md-12 whotitle"> <h3>Who we are</h3></div>
+                 <div class="col-md-6">
+                 <img src="images/why.png" alt="Image" class="whyimage">
 
-    <div class="custom-breadcrumns border-bottom">
-      <div class="container">
-        <a href="#">Home</a>
-        <span class="mx-3 icon-keyboard_arrow_right"></span>
-        <span class="current">About Us</span>
-      </div>
-    </div>
-
-    <div class="container pt-3 mb-5">
-      <div class="col-md-12">
-
-        <section class="section">
-          <div class="row">
-            <div class="col-md-12 text-center mb-3">
-              <h2 class="section-title-underline">
-                <span>Who we are?</span>
-              </h2>
-            </div>
-            <div class="col-md-6 mb-lg-0 mb-4">
-              <div class="img-container">
-                <img src="http://svjnanakendra.com/assets/images/background6.jpg" alt="Image" class="img-fluid">
-              </div>
-            </div>
-            <div class="col-md-6 mb-lg-0 mb-4">
-              <p>
-                ‘Swami Vivekananda Jnana Kendra’ brain child of an Engineer by qualification, but teacher by Passion. An Educational & Training Services organization catering to needs of youngsters in Hennur Cross and adjacent localities, with ‘Quality @ Affordable Price’— an idea that itself is Philanthropic, isn’t it?Though we are not NGO, The assurance of quality teaching with utmost care to supplement knowledge transformation in true form by catering to all sections of society ranging from school dropouts to professionals in various disciplines in it is a service.The only thing that interferes with my learning is my education. (Albert Einstein) “All who have meditated on the art of governing mankind have been convinced that the fate of empires depends on the education of youth.” (Aristotle).
+                 </div>
+                 <div class="col-md-6">
+                 <p>
+                ‘Swami Vivekananda Jnana Kendra’ brain child of an Engineer by qualification, but teacher by Passion. An Educational & Training Services organization catering to needs of youngsters in Hennur Cross and adjacent localities, with ‘Quality @ Affordable Price’— an idea that itself is Philanthropic, isn’t it?Though we are not NGO, The assurance of quality teaching with utmost care to supplement knowledge transformation in true form <span id="dots">...</span><span id="more">by catering to all sections of society ranging from school dropouts to professionals in various disciplines in it is a service.The only thing that interferes with my learning is my education. (Albert Einstein) “All who have meditated on the art of governing mankind have been convinced that the fate of empires depends on the education of youth.” (Aristotle)..</span></p>
+<button onclick="myFunction()" id="myBtn">Read more</button>
               </p>
-            </div>            
-          </div>
-        </section>
 
-        <section class="section">
-          <div class="row">          
-            <div class="col-md-12">
-              <div class="text-center">
-                <h2 class="section-title-underline">
+                 </div>
+
+               </div>
+
+             </div>
+           </section>
+    
+
+
+        <section class="section misssectioN">
+          <div class="conrtainer ">
+
+        <div class="row">          
+            <div class="col-md-12 ">
+              <div class="text-center misstit">
+                <h2 class="">
                   <span>Our Mission</span>
                 </h2>
               </div>
-              <div class="row">
-                <div class="col-md-6 mobile-view-img">
-                  <div class="img-container">
-                    <img src="images/OurVision.png" alt="Image" class="img-fluid">
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div>
+              <div class="row justify-content-center">
+                <div class="col-md-5 misstext" >
+                  
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -81,30 +69,32 @@
                       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                  </div>
+                  
                 </div>
-                <div class="col-md-6 desktop-view-img">
+                <div class="col-md-6">
                   <div class="img-container">
-                    <img src="images/OurVision.png" alt="Image" class="img-fluid">
+                    <img src="images/miss.png" alt="Image" class="img-fluid">
                   </div>
                 </div>
-              </div>
-            </div><!--end mission col-md-12-->
+              </div></div>
+            </div>
+            <!--end mission col-md-12-->
+            <div class="container">
             <div class="col-md-12">
-              <div class="text-center">
-                <h2 class="section-title-underline">
+              <div class="text-center misstit">
+                <h2 class="">
                   <span>Our Vision</span>
                 </h2>
               </div>
               <div class="row">
                 <div class="col-md-6">
                   <div class="img-container">
-                    <img src="images/vision.gif" alt="Image" class="img-fluid">
+                    <img src="images/mi.png" alt="Image" class="img-fluid">
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div>
-                    <p>
+                    <p class="vistext">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -115,7 +105,7 @@
                   </div>
                 </div>
               </div>
-            </div><!--end vision col-md-12-->
+            </div> </div><!--end vision col-md-12-->
 
           </div>
         </section>
@@ -134,6 +124,24 @@
 
   <?php include 'script_links.php'; ?>
 
+<script>
+  
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+</script>
 </body>
 
 </html>
--- navigation.php
+++ navigation.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/cutom.css">  
+	
+	<title>Document</title>
+</head>
+<body>
+<?php include 'head_links.php'; ?>
 <div class="site-mobile-menu site-navbar-target">
 	<div class="site-mobile-menu-header">
 		<div class="site-mobile-menu-close mt-3">
@@ -7,34 +21,33 @@
 	<div class="site-mobile-menu-body"></div>
 </div>
 
-<div class="py-2 top">
-	<div class="container">
-		<div class="row align-items-center">
+<div class=" top">
+	<div class="container-fluid headerow">
+		<div class="row align-items-center ">
 			<div class="col-lg-9 d-none d-lg-block">
-				<a href="#" class="small mr-3"><span class="icon-question-circle-o mr-2"></span> Have a questions?</a> 
-				<a href="tel://+91 9731263208" class="small mr-3"><span class="icon-phone2 mr-2"></span> +91 9731263208</a> 
-				<a href="#" class="small mr-3"><span class="icon-envelope-o mr-2"></span> svj.kendra@gmail.com</a> 
+				<a href="tel://+91 9731263208" class="small mr-3"><span class="icon-phone2 mr-2"></span> <span>+91 9731263208</span></a> 
+				<a href="#" class="small mr-3"><span class="icon-envelope-o mr-2"></span> <span>svj.kendra@gmail.com</span> </a> 
 			</div>
-			<div class="col-lg-3 text-right">
+			<div class="col-lg-3 text-right" style="color:red;">
 				<!-- <a href="login.php" class="small mr-3"><span class="icon-unlock-alt"></span> Log In</a> -->
-				<a href="#" class="small btn btn-primary px-4 py-2 rounded-0"><span class="icon-users"></span> Register</a>
+				<a href="#"  class="regiter "><span class="icon-users"></span> <span>Register</span></a>
 			</div>
 		</div>
 	</div>
 </div>
 
 
-<header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+<header class="site-navbar js-sticky-header " role="banner">
 
-	<div class="container">
+	<div class="container-fluid navigation">
 		<div class="d-flex align-items-center">
 			<div class="site-logo">
 				<a href="index.php" class="d-block">
-					<img src="images/svjk_logo.png" alt="Swami Vivekananda Jnana Kendra" class="img-fluid" style="height: 3.5rem;">
+					<img src="images/svjk_logo.png" alt="Swami Vivekananda Jnana Kendra" class="img-fluid">
 				</a>
 			</div>
 			<div class="ml-auto">
-				<nav class="site-navigation position-relative text-right" role="navigation">
+				<nav class=" position-relative text-right" role="navigation">
 					<ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
 						<li class="">
 							<a href="index.php" class="nav-link text-left">Home</a>
@@ -73,3 +86,5 @@
 		</div>
 
 	</header>
+</body>
+</html>
